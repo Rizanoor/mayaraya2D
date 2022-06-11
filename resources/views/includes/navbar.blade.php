ad16ea33968b54a67bf516ac7a691b6dfe262307
Rizanoor <rizanoorfauzan123@gmail.com>
2022-06-11 12:44:08 +0700

    rapihin navbar

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -1,80 +1,82 @@
-<!-- Header -->
 <section class="h-100 w-100" style="box-sizing: border-box; 
-background-image: url('images/img_background.png');
-background-size: cover;
-background-repeat: no-repeat;
-background-attachment: fixed;
-background-position: center; 
-">
-<div class="container-xxl mx-auto p-0  position-relative header-2-1">
-<nav class="navbar navbar-expand-lg navbar-light">
-<a href="#">
-<img style="margin-right: 0.75rem; width: 100px;" src="images/img_logo.png" alt="" data-aos="fade-down" />
-</a>
-<button class="navbar-toggler border-0" type="button" data-bs-toggle="modal" data-bs-target="#targetModal-item">
-<span class="navbar-toggler-icon"></span>
-</button>
+                                    background-image: url('images/img_background.png');
+                                    background-size: cover;
+                                    background-repeat: no-repeat;
+                                    background-attachment: fixed;
+                                    background-position: center; 
+                                    ">  
+  <div class="container-xxl mx-auto p-0  position-relative header-2-1">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <a href="#">
+          <img style="margin-right: 0.75rem; width: 100px;" src="images/img_logo.png" alt="" data-aos="fade-down" />
+        </a>
+        <button class="navbar-toggler border-0" type="button" data-bs-toggle="modal" data-bs-target="#targetModal-item">
+          <span class="navbar-toggler-icon"></span>
+        </button>
 
-<div class="modal-item modal fade" id="targetModal-item" tabindex="-1" role="dialog"
-aria-labelledby="targetModalLabel" aria-hidden="true">
-<div class="modal-dialog" role="document">
-<div class="modal-content border-0" style="background-color: rgb(240, 176, 170) ;">
-<div class="modal-header border-0" style="padding: 2rem; padding-bottom: 0">
-<a class="modal-title" id="targetModalLabel">
-<img style="margin-top: 0.5rem; width: 50px;" src="images/img_logo.png" alt="" />
-</a>
-<button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-</div>
-<div class="modal-body" style="padding: 2rem; padding-top: 0; padding-bottom: 0">
-<ul class="navbar-nav responsive me-auto mt-2 mt-lg-0">
-<li class="nav-item active">
-<a class="nav-link" href="">Beranda</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="#" style="color: #E9E9E9;">Tentang</a>
-</li>
-<li class="nav-item">
-<a class="nav-link" href="profile.html" style="color: #E9E9E9;">Profil</a>
-</li>
-</ul>
-</div>
-<div class="modal-footer border-0 gap-3" style="padding: 2rem; padding-top: 0.75rem">
-<a href="login.html" class="btn btn-default btn-no-fill">
-Masuk
-</a>
-<a href="../index.html" class="btn btn-fill text-white">3D</a>
-</div>
-</div>
-</div>
-</div>
+        <div class="modal-item modal fade" id="targetModal-item" tabindex="-1" role="dialog"
+          aria-labelledby="targetModalLabel" aria-hidden="true">
+          <div class="modal-dialog" role="document">
+            <div class="modal-content border-0" style="background-color: rgb(240, 176, 170) ;">
+              <div class="modal-header border-0" style="padding: 2rem; padding-bottom: 0">
+                <a class="modal-title" id="targetModalLabel">
+                  <img style="margin-top: 0.5rem; width: 50px;" src="images/img_logo.png" alt="" />
+                </a>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body" style="padding: 2rem; padding-top: 0; padding-bottom: 0">
+                <ul class="navbar-nav responsive me-auto mt-2 mt-lg-0">
+                    <li class="nav-item active">
+                      <a class="nav-link" href="">Beranda</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="#" style="color: #E9E9E9;">Tentang</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="profile.html" style="color: #E9E9E9;">Profil</a>
+                    </li>
+                </ul>
+              </div>
+              <div class="modal-footer border-0 gap-3" style="padding: 2rem; padding-top: 0.75rem">
+                <a href="login.html" class="btn btn-default btn-no-fill">
+                  Masuk
+                </a>
+              <a href="../index.html" class="btn btn-fill text-white">3D</a>
+              </div>
+            </div>
+          </div>
+        </div>
 
-<div class="collapse navbar-collapse" id="navbarTogglerDemo" data-aos="fade-down">
-<ul class="navbar-nav me-auto mt-2 mt-lg-0">
-<li class="nav-item active">
-<a href="#" class="nav-link" href="#">Beranda</a>
-</li>
-<li class="nav-item">
-<a href="#tentang" class="nav-link" href="#">Tentang</a>
-</li>
-<li class="nav-item">
-<a href="profile.html" class="nav-link" href="#">Profil</a>
-</li>
-</ul>
-<div class="gap-3">
-<a href="login.html" class="btn btn-default btn-no-fill">
-Masuk
-</a>
-<a href="../index.html" class="btn btn-fill text-white">3D</a>
-</div>
-</div>
-</nav>
+        <div class="collapse navbar-collapse" id="navbarTogglerDemo" data-aos="fade-down">
+          <ul class="navbar-nav me-auto mt-2 mt-lg-0">
+            <li class="nav-item active">
+              <a href="#" class="nav-link" href="#">Beranda</a>
+            </li>
+            <li class="nav-item">
+              <a href="#tentang" class="nav-link" href="#">Tentang</a>
+            </li>
+            <li class="nav-item">
+              <a href="profile.html" class="nav-link" href="#">Profil</a>
+            </li>
+          </ul>
+          <div class="gap-3">
+            <a href="login.html" class="btn btn-default btn-no-fill">
+              Masuk
+            </a>
+            <a href="../index.html" class="btn btn-fill text-white">
+              3D
+            </a>
+          </div>
+        </div>
+      </nav>
 
-<!-- Image-Landmark -->
-<section class="h-100 w-100">
-<div class="empty-3-1" data-aos="zoom-in">
-<div class="mx-auto d-flex align-items-center justify-content-center flex-column">
-<img class="main-img" src="images/img_landmark.png" alt="">
-</div>
-</div>
-</section>
+      <!-- Image-Landmark -->
+      <section class="h-100 w-100">
+        <div class="empty-3-1" data-aos="zoom-in">
+          <div class="mx-auto d-flex align-items-center justify-content-center flex-column">
+           <img class="main-img" src="images/img_landmark.png" alt="">
+          </div>
+        </div>
+    </section>
+  </div>
 </section>
